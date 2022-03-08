5bcf1333b16eabfb9dd78f1d1e87455b535d4b01
comav <tsyomav@gmail.com>
2022-03-09 01:13:28 +0200

    Added an auth screen (not functional)

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
+import react from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+import { Provider } from 'react-redux';
+import configureStore from './redux/store';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import authScreen from './screens/authScreen';
+
+const Stack = createNativeStackNavigator();
+const store = configureStore();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+           name='Auth' 
+           component={authScreen} 
+           options={{
+             headerShown: false
+           }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
